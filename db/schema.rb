085967f8858ad6f18fb5d63d1e9b3082b81a397b
junichi kinami <junichi.kinami@gmail.com>
2024-08-03 16:02:54 +0900

    Created seedfile to generate 10 anime from MAL api

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_02_134108) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "myanimelist_username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
