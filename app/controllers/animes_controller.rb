d5be7c23498e69b889f21e0700c628f37ffe7ced
michael stump <maikerustump@gmail.com>
2024-08-07 22:20:10 +0900

    created cards for popular animes

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -2,6 +2,6 @@ class AnimesController < ApplicationController
   def index
     @animes = Anime.all
     best_anime = @animes.sort_by {|anime| anime.rating}.reverse
-    @random_popular_three = best_anime.sample(3)
+    @random_popular_three = best_anime[0..5].sample(3)
   end
 end
