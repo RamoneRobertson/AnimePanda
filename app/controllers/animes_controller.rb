b60f5c3ddb06c7550a99a614f8984af9c61afa46
Ramone Robertson <ramonerobertson10@gmail.com>
2024-08-08 20:15:42 +0900

    resolving conflicts

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,5 +1,4 @@
 class AnimesController < ApplicationController
-<<<<<<< HEAD
   def recommendations
     # if there is a prompt from chatgpt
     # Get the 5 anime from the params
@@ -7,10 +6,8 @@ class AnimesController < ApplicationController
     @likes = []
     # If the user swip
   end
-=======
   skip_before_action :authenticate_user!, only: [:index]
 
->>>>>>> master
   def index
     @animes = Anime.all
     best_anime = @animes.sort_by {|anime| anime.rating}.reverse
