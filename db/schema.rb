49ebcd1996c9823a6c7f9bcd0dd279981325ed5b
junichi kinami <junichi.kinami@gmail.com>
2024-08-13 20:38:40 +0900

    Added trailer URL into seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_142040) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_13_112809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_142040) do
     t.string "studio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "trailer"
   end
 
   create_table "bookmarks", force: :cascade do |t|
