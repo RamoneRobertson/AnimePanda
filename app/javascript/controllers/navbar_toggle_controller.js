9105b2cf6bffc5caa8eee9f0d45c3e67040d24a4
michael stump <maikerustump@gmail.com>
2024-08-13 22:41:59 +0900

    adjusted the banner above the navbar

--- app/javascript/controllers/navbar_toggle_controller.js
+++ app/javascript/controllers/navbar_toggle_controller.js
@@ -3,14 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar-toggle"
 export default class extends Controller {
   static targets = [ "navToggle", "innerElements" ]
+  static values = { user: Boolean }
 
   connect() {
     // console.log("hello from navbar toggle controller");
+    // console.log(this.userValue);
   }
 
   fire(){
     // console.log(this.navToggleTarget);
-    this.navToggleTarget.classList.toggle("nav-toggle-active");
+    if(this.userValue){
+      this.navToggleTarget.classList.toggle("nav-toggle-user-active");
+    }else{
+      this.navToggleTarget.classList.toggle("nav-toggle-active");
+    }
 
     setTimeout(() => {
       this.innerElementsTargets.forEach(element => {
