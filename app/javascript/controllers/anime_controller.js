02941b0949bdf5180fbd357ff0a14b9eea1ede3b
Seisuke Fukuda <razerrmc163@gmail.com>
2024-08-19 16:40:23 +0900

    added play button and redirects to the trailer when c;icked

--- app/javascript/controllers/anime_controller.js
+++ app/javascript/controllers/anime_controller.js
@@ -1,47 +1,37 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["synopsis", "content", "toggleButton", "iframeContainer", "playButton"];
-
-  toggle() {
-    const content = this.contentTarget;
-    const button = this.toggleButtonTarget;
-
-    if (content.classList.contains("expanded")) {
-      content.classList.remove("expanded");
-      button.textContent = "Read More";
-    } else {
-      content.classList.add("expanded");
-      button.textContent = "Read Less";
-    }
-  }
+  static targets = ["iframeContainer", "playButton", "playText"];
 
   play() {
     const iframeContainer = this.iframeContainerTarget;
     const iframe = this.iframeContainerTarget.querySelector('iframe');
     const playButton = this.playButtonTarget;
+    const playText = this.playTextTarget;
 
-    // Toggle display to push down content
+    // Toggle display of the iframe container
     if (iframeContainer.style.display === "none" || iframeContainer.style.display === "") {
       iframeContainer.style.display = "block";
 
-      // Add a short delay before reloading the iframe to ensure it's visible when reloaded
-      setTimeout(() => {
-        // Reload the iframe to enforce autoplay
-        const src = iframe.src;
-        iframe.src = ''; // Temporarily clear the src
-        iframe.src = src; // Reset the src to force reload with autoplay
-      }, 100);
+      // Scroll to iframe container
+      iframeContainer.scrollIntoView({ behavior: "smooth", block: "start" });
 
-      // Change play icon to pause icon
+      // Set iframe src to start playing video
+      const src = iframe.src;
+      iframe.src = '';
+      iframe.src = src;
+
+      // Change play button icon and text to pause
       playButton.classList.remove("fa-play");
       playButton.classList.add("fa-pause");
+      playText.textContent = "Hide Trailer";
     } else {
       iframeContainer.style.display = "none";
 
-      // Change pause icon back to play icon
+      // Change pause button icon and text back to play
       playButton.classList.remove("fa-pause");
       playButton.classList.add("fa-play");
+      playText.textContent = "Play Trailer";
     }
   }
 }
