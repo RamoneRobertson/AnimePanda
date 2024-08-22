dc71f38ece40a867f583db782b038d092d78451c
Ramone Robertson <ramonerobertson10@gmail.com>
2024-08-22 22:03:18 +0900

    pulling changes

--- app/javascript/controllers/panda_comment_controller.js
+++ app/javascript/controllers/panda_comment_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="panda-comment"
 export default class extends Controller {
   connect() {
+    console.log("connected to panda comment ")
   }
 }
